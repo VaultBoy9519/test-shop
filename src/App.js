ebebaa395e19bb7ad6abcd8f5c8098390fe85aeb
VaultBoy9519 <illidan9519@mail.ru>
2023-03-07 15:48:51 +0300

    Созданы компоненты Header и Drawer"

--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
 import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
     <div className="wrapper clear">
-      <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
-          <h2 className="mb-30 d-flex justify-between">Корзина
-            <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
-          </h2>
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              <div style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }} className="cartItemImg">
-              </div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
-                <b>12990 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-            </div>
-            <div className="cartItem d-flex align-center mb-20">
-              <div style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }} className="cartItemImg">
-
-              </div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
-                <b>12990 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-            </div>
-          </div>
-          <div className="cartTotalBlock">
-            <ul className="cartTotalBlock">
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21498 руб.</b>
-              </li>
-              <li>
-                <span>Налог 5%</span>
-                <div></div>
-                <b>1074 руб.</b>
-              </li>
-            </ul>
-            <button className="greenButton">Оформить заказ<img src="/img/arrow.svg" alt="Arrow" /></button>
-          </div>
-        </div>
-      </div>
-
-      <header className="d-flex justify-between align-center p-40">
-        <div className="headerLeft d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" />
-          <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
-          </div>
-        </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" />
-            <span>1205 руб.</span>
-          </li>
-          <li>
-            <img width={18} height={18} src="/img/user.svg" />
-          </li>
-        </ul>
-      </header>
+      <Drawer />
+      <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
